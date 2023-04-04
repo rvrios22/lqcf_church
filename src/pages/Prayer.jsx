a05627b84ba883e07b6c6758b038f1c71ddf2c9d
Rafael Rios <rvrios22@gmail.com>
2023-04-04 06:14:08 -0700

    add: form on prayer page

--- src/pages/Prayer.jsx
+++ src/pages/Prayer.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import emailjs from "@emailjs/browser";
 
 export default function Prayer() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMLATE_ID,
+        form.current,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then(
+        (res) => {
+          console.log(res.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div>
       <h2>Why Should We Pray?</h2>
@@ -18,8 +42,19 @@ export default function Prayer() {
 
       <div>
         <h3>Are You In Need of Prayer?</h3>
-        <p>Please fill out our form to send us your prayer request and we will pray for you.</p>
-        
+        <p>
+          Please fill out our form to send us your prayer request and we will
+          pray for you.
+        </p>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
     </div>
   );
